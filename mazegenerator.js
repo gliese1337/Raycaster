0c236133e6acc91f23418bc51054b32bcadbe7cd
Logan Kearsley <chronosurfer@gmail.com>
2014-06-11 12:35:16 -0600

    Implement 3D world.

--- mazegenerator.js
+++ mazegenerator.js
@@ -6,9 +6,9 @@ var Maze = (function() {
 
 	function randomDirections() {
 		var i, j, list, tmp;
-		list = ['N', 'S', 'E', 'W'];
-		for (i = 0; i < 4; i++) {
-		  j = i + rand(4 - i);
+		list = ['N', 'S', 'E', 'W', 'U', 'D'];
+		for (i = 0; i < 6; i++) {
+		  j = i + rand(6 - i);
 		  tmp = list[j]
 		  list[j] = list[i]
 		  list[i] = tmp;
@@ -16,84 +16,100 @@ var Maze = (function() {
 		return list;
 	};
 	
-	function Maze(width, height) {
-		var i, x, y, nx, ny,
-			grid, row,
+	function Maze(width, height, depth) {
+		var i, x, y, z, nx, ny, nz,
+			grid, level, row,
 			cell, cells,
 			index, dirs;
 
 		grid = [];
-		for (y = 0; y < height * 2; y++) {
-			row = [];
-			for (x = 0; x < width * 2; x++) { row.push(1); }
-			grid.push(row);
+		for (z = 0; z < depth * 2; z++){
+			level = [];
+			for (y = 0; y < height * 2; y++) {
+				row = [];
+				for (x = 0; x < width * 2; x++) { row.push(1); }
+				level.push(row);
+			}
+			grid.push(level);
 		}
 
 		nx = rand(width - 1) * 2 + 1;
 		ny = rand(height - 1) * 2 + 1;
-		grid[ny][nx] = 0;
-		cells = [{x:nx,y:ny}];
+		nz = rand(depth - 1) * 2 + 1;
+		grid[nz][ny][nx] = 0;
+		cells = [{x:nx,y:ny,z:nz}];
 
 		while (cells.length > 0) {
 			index = Math.random() < .5 ? rand(cells.length) : cells.length - 1;
 			cell = cells[index];
 			dirs = randomDirections();
-			loop: for (i = 0; i < 4; i++) {
+			loop: for (i = 0; i < 6; i++) {
 				switch(dirs[i]){
 				case 'N':
 					nx = cell.x;
 					ny = (cell.y - 2 + height*2)%(height*2);
-					if (grid[ny][nx] === 1) {
-						grid[(ny + 1)%(height*2)][nx] = 0;
+					nz = cell.z;
+					if (grid[nz][ny][nx] === 1) {
+						grid[nz][(ny + 1)%(height*2)][nx] = 0;
 						break loop;
 					}
 					break;
 				case 'S':
 					nx = cell.x;
 					ny = (cell.y + 2)%(height*2);
-					if (grid[ny][nx] === 1) {
-						grid[(ny - 1 + height*2)%(height*2)][nx] = 0;
+					nz = cell.z;
+					if (grid[nz][ny][nx] === 1) {
+						grid[nz][(ny - 1 + height*2)%(height*2)][nx] = 0;
 						break loop;
 					}
 					break;
 				case 'E':
 					nx = (cell.x + 2)%(width*2);
 					ny = cell.y;
-					if (grid[ny][nx] === 1) {
-						grid[ny][(nx - 1 + width*2)%(width*2)] = 0;
+					nz = cell.z;
+					if (grid[nz][ny][nx] === 1) {
+						grid[nz][ny][(nx - 1 + width*2)%(width*2)] = 0;
 						break loop;
 					}
 					break;
 				case 'W':
 					nx = (cell.x - 2 + width*2)%(width*2);
 					ny = cell.y;
-					if (grid[ny][nx] === 1) {
-						grid[cell.y][(nx + 1)%(width*2)] = 0;
+					nz = cell.z;
+					if (grid[nz][ny][nx] === 1) {
+						grid[nz][ny][(nx + 1)%(width*2)] = 0;
+						break loop;
+					}
+					break;
+				case 'U':
+					nx = cell.x;
+					ny = cell.y;
+					nz = (cell.z + 2)%(depth*2);
+					if (grid[nz][ny][nx] === 1) {
+						grid[(nz - 1 + depth*2)%(depth*2)][ny][nx] = 0;
+						break loop;
+					}
+					break;
+				case 'D':
+					nx = cell.x
+					ny = cell.y;
+					nz = (cell.z - 2 + depth*2)%(depth*2);;
+					if (grid[nz][ny][nx] === 1) {
+						grid[(nz + 1)%(depth*2)][ny][nx] = 0;
 						break loop;
 					}
 					break;
 				}
 			}
-			if(i === 4){ cells.splice(index, 1); }
+			if(i === 6){ cells.splice(index, 1); }
 			else{
-				grid[ny][nx] = 0;
-				cells.push({ x: nx, y: ny });
+				grid[nz][ny][nx] = 0;
+				cells.push({ x: nx, y: ny, z: nz });
 			}
 		}
-		grid[rand(height - 1)*2 + 1][0] = 0;
+		grid[rand(depth - 1)*2 + 1][rand(height - 1)*2 + 1][0] = 0;
 		return grid;
 	}
-	
-	Maze.print = function(grid, height, width){
-		var str = "\n";
-		for(y = 0; y < height * 2; y++){
-			for(x = 0; x < width * 2; x++){
-				str += grid[y][x] ? '#' : '+';
-			}
-			str += '\n';
-		}
-		return str;
-	};
 	
 	return Maze;
 })();
